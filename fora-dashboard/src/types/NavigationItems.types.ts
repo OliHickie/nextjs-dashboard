1b1297df57dd413e837e5f3f8871f28e5acddf28
Oli Hickie <oli.h@hotmail.co.uk>
2025-05-01 19:45:29 +0100

    Create type files for Tickets and Navigation Items

---
+++ fora-dashboard/src/types/NavigationItems.types.ts
@@ -0,0 +1,12 @@
+export type MainNavItem = {
+  href: string;
+  icon: string;
+  label: string;
+  size: number;
+};
+
+export type SecondaryNavItem = {
+  icon: string;
+  label: string;
+  size: number;
+};  
---
+++ fora-dashboard/src/types/Tickets.types.ts
@@ -0,0 +1,8 @@
+export type Ticket = {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  priority: string;
+  createdAt: string;
+};
