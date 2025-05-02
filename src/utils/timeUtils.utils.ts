f5c61b3801e08f5c99c6676882998582531a0867
Oli Hickie <oli.h@hotmail.co.uk>
2025-05-02 16:46:34 +0100

    Add timeAgo function into time.utils file

---
+++ src/utils/timeUtils.utils.ts
@@ -0,0 +1,17 @@
+export const timeAgo = (dateInput: string | Date): string => {
+  const now = new Date();
+  const past = new Date(dateInput);
+  const diffMs = now.getTime() - past.getTime();
+
+  if (diffMs <= 0) return '0 hours';
+
+  const hours = Math.floor(diffMs / (1000 * 60 * 60));
+  const days = Math.floor(hours / 24);
+
+  if (hours < 24) {
+    return `${hours} hour${hours !== 1 ? 's' : ''}`;
+  } else {
+    return `${days} day${days !== 1 ? 's' : ''}`;
+  }
+}
+
