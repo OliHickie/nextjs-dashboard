857785f7264a886d900e8524803c65026227dc60
Oli Hickie <oli.h@hotmail.co.uk>
2025-05-02 11:39:34 +0100

    Fix build errors
    Update deprecated @import to @use

--- fora-dashboard/src/services/getTickets.service.ts
+++ fora-dashboard/src/services/getTickets.service.ts
@@ -1,7 +1,7 @@
 import { Ticket } from "@/types/Tickets.types";
-import { tickets } from "@/data/IssueForce.json";
+import tickets  from "@/data/IssueForce.json";
 
 export const getTickets = async (): Promise<Ticket[]> => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  return tickets;
+  return tickets.tickets;
 };
