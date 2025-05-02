7056e07c574ac80735bca15fb4ed4e9ed4e1ae72
Oli Hickie <oli.h@hotmail.co.uk>
2025-05-02 16:52:20 +0100

    Update IssueForce data for testing

--- src/services/getTickets.service.ts
+++ src/services/getTickets.service.ts
@@ -3,5 +3,5 @@ import tickets  from "@/data/IssueForce.json";
 
 export const getTickets = async (): Promise<Ticket[]> => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  return tickets.tickets;
+  return tickets.data;
 };
