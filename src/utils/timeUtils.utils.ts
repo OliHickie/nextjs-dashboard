110f5e1db95a15627f5d9c4526d9f9cd8c1bb462
Oli Hickie <oli.h@hotmail.co.uk>
2025-05-04 14:50:22 +0100

    Add format date util

--- src/utils/timeUtils.utils.ts
+++ src/utils/timeUtils.utils.ts
@@ -1,3 +1,4 @@
+// returns the time difference between now and the date input in hours or days
 export const timeAgo = (dateInput: string | Date): string => {
   const now = new Date();
   const past = new Date(dateInput);
@@ -22,9 +23,30 @@ export const sortByDate = (a: string | Date, b: string | Date): number => {
   return dateB - dateA;
 }
 
+// returns 08:30
 export const formatTime = (dateInput: string): string => {
   const date = new Date(dateInput);
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
   return `${hours}:${minutes}`;
 }
+
+// returns Thursday 19th
+export const formatDayOfWeek = (dateInput: string | Date): string => {
+  const date = new Date(dateInput);
+
+  const weekday = new Intl.DateTimeFormat('en-GB', { weekday: 'long' }).format(date);
+  const day = date.getDate();
+
+  const getOrdinal = (n: number): string => {
+    if (n >= 11 && n <= 13) return `${n}th`;
+    switch (n % 10) {
+      case 1: return `${n}st`;
+      case 2: return `${n}nd`;
+      case 3: return `${n}rd`;
+      default: return `${n}th`;
+    }
+  };
+
+  return `${weekday} ${getOrdinal(day)}`;
+};
